cca905004bf158f26802dc6fc26a20aa28117c7a
zetro <83398581+zetrobt@users.noreply.github.com>
2023-07-03 01:15:55 +0500

    Create tgapi.py

---
+++ tgapi.py
@@ -0,0 +1,36 @@
+from requests import get
+from time import sleep
+
+class TgApi:
+	def __init__(self, token=None):
+		self.handlers = []
+		self.last_update = 0
+		if not token:
+			print("Insert bot token!")
+			raise SystemError
+		else:
+			self.token=token
+	
+	def _exec_task(self, task, *args, **kwargs):
+	           task(*args, **kwargs)
+	
+	def polling(self, infinity=False):
+		while True:
+			updates = get(f"https://api.telegram.org/bot{self.token}/getUpdates?offset={self.last_update}").json()
+			for update in updates["result"]:
+				print(update)
+				if update["message"]:
+					for handler in self.handlers:
+						if update["message"]["text"] == handler.command:
+							self.last_update = int(update["update_id"]) + 1
+							handler.callback(update["message"])
+			sleep(0.25)
+	
+	def create_handler(self, handler=None):
+		if not handler:
+			print("Error! Pass handler to create_handler")
+			return
+		self.handlers.append(handler)
+	
+	def send_message(self, chat_id, text):
+	           get(f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={chat_id}&text={text}")

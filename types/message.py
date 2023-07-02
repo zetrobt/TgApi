2609000a394d2eea01eeaeca239fe19d4e344838
zetro <83398581+zetrobt@users.noreply.github.com>
2023-07-03 02:50:02 +0500

    Update message.py
    Added message ID

--- types/message.py
+++ types/message.py
@@ -1,5 +1,6 @@
 class Message:
 	def __init__(self, message):
+		self.id = message["message_id"]
 		self.user_id = message["from"]["id"]
 		self.chat_id = message["chat"]["id"]
 		self.text = message["text"]

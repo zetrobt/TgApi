2b733b0ab86ab86b6bc16a5236ecb97c213b03ae
zetro <83398581+zetrobt@users.noreply.github.com>
2023-07-03 15:47:04 +0500

    Update tgapi.py
    New methods and object returning

--- tgapi.py
+++ tgapi.py
@@ -1,7 +1,9 @@
-from requests import get
+from requests import Session, get
 from time import sleep
-from .types.message import Message
+from json import dumps
+from .types import Message, Actions
 from .handlers import Handler
+from .utils import *
 
 class TgApi:
 	def __init__(self, token=None):
@@ -12,15 +14,17 @@ class TgApi:
 			raise SystemError
 		else:
 			self.token=token
+			self.rq = Session(base_url=f"https://api.telegram.org/bot{token}/")
 	
 	def _exec_task(self, task, *args, **kwargs):
 	           task(*args, **kwargs)
 	
 	def polling(self, infinity=False):
 		while True:
-			updates = get(f"https://api.telegram.org/bot{self.token}/getUpdates?offset={self.last_update}").json()["result"]
+			updates = self.rq.get(f"getUpdates?offset={self.last_update}").json()["result"]
+			print(updates)
 			for update in updates:
-				print(update)
+				#print(update)
 				if update["message"]:
 					message = Message(update["message"])
 					for handler in self.handlers:
@@ -43,8 +47,21 @@ class TgApi:
 			return
 		self.handlers.append(handler)
 	
-	def send_message(self, chat_id, text):
-		get(f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={chat_id}&text={text}")
+	def send_message(self, chat_id, text, reply_markup=None):
+		if not reply_markup:
+			return Message(self.rq.get(f"sendMessage?chat_id={chat_id}&text={text}").json()["result"])
+		return Message(self.rq.get(f"sendMessage?chat_id={chat_id}&text={text}&reply_markup={reply_markup}").json()["result"])
 	
-	def reply(self, message, text):
-		get(f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={message.chat_id}&text={text}&reply_to_message_id={message.id}")
+	def reply_message(self, message, text, reply_markup=None):
+		if not reply_markup:
+			return Message(self.rq.get(f"sendMessage?chat_id={message.chat_id}&text={text}&reply_to_message_id={message.id}").json()["result"])
+		return Message(self.rq.get(f"sendMessage?chat_id={message.chat_id}&text={text}&reply_to_message_id={message.id}&reply_markup={reply_markup}").json()["result"])
+	
+	def edit_message(self, chat_id, message_id, text):
+		self.rq.get(f"editMessageText?chat_id={chat_id}&message_id={message_id}&text={text}")
+	
+	def forward_message(self, from_chat_id, message_id, chat_id):
+		self.rq.get(f"forwardMessage?from_chat_id={from_chat_id}&message_id={message_id}&chat_id={chat_id}")
+	
+	def send_action(self, chat_id, action):
+		self.rq.get(f"sendChatAction?chat_id={chat_id}&action={action}")

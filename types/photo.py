018d77eba25502b397b43e31405965be32dd42d0
zetro <83398581+zetrobt@users.noreply.github.com>
2023-07-06 14:20:49 +0500

    Update photo.py

--- types/photo.py
+++ types/photo.py
@@ -1,5 +1,7 @@
 class Photo:
 	def __init__(self, photo):
-		self.small_size_id = photo[0]["file_id"]
-		self.normal_size_id = photo[1]["file_id"]
-		self.large_size_id = photo[2]["file_id"]
+		self.file_id = photo["file_id"]
+		self.file_unique_id = photo["file_unique_id"]
+		self.width = photo["width"]
+		self.height = photo["height"]
+		

df32db7e32a413fd827be9880a4160755351eb5b
zetro <83398581+zetrobt@users.noreply.github.com>
2023-07-16 16:36:05 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TgApi",
+    version="1.6",
+    description="Experimental library for working with Telegram in Python",
+    url="https://github.com/zetrobt/TgApi",
+    download_url="https://github.com/zetrobt/TgApi/releases/latest",
+    author="zetro.",
+    author_email="me@zetro.ga",
+    license="MIT",
+    python_requires="~=3.7",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=["requests"]
+)
